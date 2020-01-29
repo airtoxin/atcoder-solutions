a9aea81699bc0c95bdc99ad369e94e608e5070bb
airtoxin <airtoxin@icloud.com>
2020-01-29 14:58:09 +0900

    Add solution for ABC085C

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,5 +1,21 @@
+use std::cmp::min;
+
 fn main() {
-    unimplemented!();
+    let line = read_vec::<i32>();
+    let n = line[0];
+    let y = line[1];
+
+    for num_10000 in (0..n + 1).rev() {
+        for num_5000 in (0..n - num_10000 + 1).rev() {
+            let num_1000 = n - num_10000 - num_5000;
+            if num_10000 * 10000 + num_5000 * 5000 + num_1000 * 1000 == y {
+                println!("{} {} {}", num_10000, num_5000, num_1000);
+                return ();
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
 
 pub fn read<T: std::str::FromStr>() -> T {
@@ -9,7 +25,10 @@ pub fn read<T: std::str::FromStr>() -> T {
 }
 
 pub fn read_vec<T: std::str::FromStr>() -> Vec<T> {
-    read::<String>().split_whitespace().map(|e| e.parse().ok().unwrap()).collect()
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
 }
 
 pub fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
