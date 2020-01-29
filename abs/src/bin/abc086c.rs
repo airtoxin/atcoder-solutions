60511afbd2d491d3afccbe3e09eaa5e7028ebf88
airtoxin <airtoxin@icloud.com>
2020-01-29 18:45:07 +0900

    Add solution for ABC086C

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,5 +1,36 @@
+use std::i32;
+
 fn main() {
-    unimplemented!();
+    let n = read::<u32>();
+    let lines = read_vec2::<i32>(n);
+
+    let mut prev_t = 0;
+    let mut prev_x = 0;
+    let mut prev_y = 0;
+
+    for line in lines {
+        let t = line[0];
+        let diff_t = t - prev_t;
+        let x = line[1];
+        let y = line[2];
+        let diff_xy = (prev_x - x).abs() + (prev_y - y).abs();
+
+        if diff_xy > diff_t {
+            println!("No");
+            return ();
+        }
+
+        if (diff_t - diff_xy) % 2 != 0 {
+            println!("No");
+            return ();
+        } else {
+            prev_t = t;
+            prev_x = x;
+            prev_y = y;
+        }
+    }
+
+    println!("Yes");
 }
 
 pub fn read<T: std::str::FromStr>() -> T {
