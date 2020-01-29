e645fd21c91398dfb37b984729f8426db3d6cb79
airtoxin <airtoxin@icloud.com>
2020-01-29 14:40:36 +0900

    Add solution for ABC085B

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,17 +1,26 @@
+use std::collections::HashSet;
+
 fn main() {
-    unimplemented!();
+    let n = read::<i32>();
+    let mut d_vec: Vec<i32> = vec![];
+    for _ in 0..n {
+        d_vec.push(read::<i32>());
+    }
+    let uniq_d: HashSet<i32> = d_vec.into_iter().collect();
+
+    println!("{}", uniq_d.len());
 }
 
-pub fn read<T: std::str::FromStr>() -> T {
+fn read<T: std::str::FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
     s.trim().parse().ok().unwrap()
 }
 
-pub fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
     read::<String>().split_whitespace().map(|e| e.parse().ok().unwrap()).collect()
 }
 
-pub fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
 }
