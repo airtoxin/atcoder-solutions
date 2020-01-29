9ac3cea60a9e7b60e9d4483022273dd1bd161570
airtoxin <airtoxin@icloud.com>
2020-01-29 14:08:32 +0900

    Add solution of ABC081A

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -1,17 +1,29 @@
 fn main() {
-    unimplemented!();
+    let s = read::<i32>();
+    println!("{}", sum_each_digit(s));
 }
 
-pub fn read<T: std::str::FromStr>() -> T {
+fn read<T: std::str::FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
     s.trim().parse().ok().unwrap()
 }
 
-pub fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
     read::<String>().split_whitespace().map(|e| e.parse().ok().unwrap()).collect()
 }
 
-pub fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
+}
+
+fn sum_each_digit(mut n: i32) -> i32 {
+    let mut sum = 0;
+
+    while n != 0 {
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    sum
 }
