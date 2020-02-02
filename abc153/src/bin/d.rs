0aee51833538710a21c0eb03b99df0eca92bc4e3
airtoxin <airtoxin@icloud.com>
2020-02-02 21:41:17 +0900

    Add solution for d

--- abc153/src/bin/d.rs
+++ abc153/src/bin/d.rs
@@ -1,5 +1,36 @@
+/*
+1→1
+==============+2
+2→3
+3→3
+==============+4
+4→7
+5→7
+6→7
+7→7
+==============+8
+8→15
+9→15
+10→15
+11→15
+12→15
+13→15
+14→15
+==============+16
+15→31
+*/
+
 fn main() {
-    unimplemented!();
+    let h = read::<i64>();
+
+    let power_of_2s: Vec<i64> = (0..40).map(|i| 2i64.pow(i)).collect();
+
+    let mut index = 0;
+    while h > power_of_2s[0..index].into_iter().sum::<i64>() {
+        index += 1;
+    }
+
+    println!("{}", power_of_2s[0..index].into_iter().sum::<i64>());
 }
 
 fn read<T: std::str::FromStr>() -> T {
