0736f0ec37f29e9cd1db5e79c53b7df1fc3f91be
airtoxin <airtoxin@icloud.com>
2020-02-03 18:54:41 +0900

    WIP c

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -1,5 +1,23 @@
+use std::cmp::max;
+
 fn main() {
-    unimplemented!();
+    let n = read::<usize>();
+    let abc_vec = read_vec2(n as u32);
+    let mut values_abc = vec![vec![0; 3]; n];
+    values_abc[0] = vec![abc_vec[0][0], abc_vec[0][1], abc_vec[0][2]];
+
+    for i in 1..n {
+        for abc_i in 0..3 {
+            let v = abc_vec[i][(abc_i + 1) % 3];
+            let w = abc_vec[i][(abc_i + 2) % 3];
+
+            values_abc[i][abc_i] = max(values_abc[i][abc_i], values_abc[i - 1][abc_i] + max(v, w));
+        }
+    }
+
+    let last = &values_abc[values_abc.len() - 1];
+    println!("{:?}", values_abc);
+    println!("{}", max(last[0], max(last[1], last[2])));
 }
 
 pub fn read<T: std::str::FromStr>() -> T {
